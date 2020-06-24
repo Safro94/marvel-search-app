518bdac24fab10ea00b7402e110f356b0d3d54f6
msafranchik <msafranchik@TEC30M>
2020-06-24 13:23:31 -0300

    Se agrega logica para obtener personaje random si la url viene vacia y obtener el personaje si se manda en el query string

--- src/hooks/application/index.jsx
+++ src/hooks/application/index.jsx
@@ -2,6 +2,8 @@ import React, { createContext, useContext, useReducer } from 'react';
 
 const SET_THEME = 'SET_THEME';
 const SET_CHARACTERS = 'SET_CHARACTERS';
+const SET_FILTERED_CHARACTERS = 'SET_FILTERED_CHARACTERS';
+const SET_SEARCH_VALUE = 'SET_SEARCH_VALUE';
 const ApplicationContext = createContext();
 
 const useApplication = () => {
@@ -16,6 +18,10 @@ const ApplicationProvider = ({ application, children }) => {
         return { ...state, theme: action.value };
       case SET_CHARACTERS:
         return { ...state, characters: action.value };
+      case SET_FILTERED_CHARACTERS:
+        return { ...state, filteredCharacters: action.value };
+      case SET_SEARCH_VALUE:
+        return { ...state, searchValue: action.value };
       default:
         return state;
     }
@@ -27,4 +33,11 @@ const ApplicationProvider = ({ application, children }) => {
   );
 };
 
-export { useApplication, ApplicationProvider, SET_THEME, SET_CHARACTERS };
+export {
+  useApplication,
+  ApplicationProvider,
+  SET_THEME,
+  SET_CHARACTERS,
+  SET_FILTERED_CHARACTERS,
+  SET_SEARCH_VALUE,
+};
--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -1,18 +1,60 @@
-import React from 'react';
+import React, { useEffect, useCallback } from 'react';
+import { useLocation } from 'react-router-dom';
 
-import { useApplication } from '../../hooks/application';
+import {
+  useApplication,
+  SET_FILTERED_CHARACTERS,
+  SET_SEARCH_VALUE,
+} from '../../hooks/application';
 
 import Card from '../../components/card';
 
 import { Wrapper } from './style';
 
 export default () => {
-  const { characters } = useApplication();
+  const { filteredCharacters, characters, setApplication } = useApplication();
+  const location = useLocation();
+
+  const getFilteredCharacters = useCallback(() => {
+    const params = new URLSearchParams(location.search);
+    const character = params.get('character');
+
+    if (character) {
+      setApplication({
+        type: SET_SEARCH_VALUE,
+        value: character,
+      });
+    }
+  }, [location.search, setApplication]);
+
+  const getRandomCharacter = useCallback(() => {
+    const randomCharacterIndex = Math.floor(
+      Math.random() * (characters.length - 1)
+    );
+    const filteredCharacters = characters[randomCharacterIndex];
+
+    setApplication({
+      type: SET_FILTERED_CHARACTERS,
+      value: [filteredCharacters],
+    });
+  }, [characters, setApplication]);
+
+  useEffect(() => {
+    if (characters && location) {
+      location.search ? getFilteredCharacters() : getRandomCharacter();
+    }
+  }, [
+    characters,
+    getFilteredCharacters,
+    getRandomCharacter,
+    location,
+    setApplication,
+  ]);
 
   return (
     <Wrapper>
-      {characters &&
-        characters.map((character) => (
+      {filteredCharacters &&
+        filteredCharacters.map((character) => (
           <Card character={character} key={character.id} />
         ))}
     </Wrapper>
