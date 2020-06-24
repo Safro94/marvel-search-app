4c7a7cfd4882f4a530baa396255225fa9e242352
msafranchik <msafranchik@TEC30M>
2020-06-24 17:37:13 -0300

    Se agrega filtro en input

--- src/components/header/search/index.jsx
+++ src/components/header/search/index.jsx
@@ -1,7 +1,33 @@
 import React from 'react';
 
+import {
+  useApplication,
+  SET_FILTERED_CHARACTERS,
+} from '../../../hooks/application';
+
+import Service from '../../../services/service';
+
 import { Input } from './style';
 
 export default () => {
-  return <Input />;
+  const { setApplication } = useApplication();
+
+  const filterCharacters = (e) => {
+    const getData = async () => {
+      const name = e.target.value.toLowerCase();
+      const characters = await Service.GetCharacters(name);
+      const newCharacters = characters.filter((character) =>
+        character.name.toLowerCase().includes(name)
+      );
+      if (newCharacters) {
+        setApplication({
+          type: SET_FILTERED_CHARACTERS,
+          value: newCharacters,
+        });
+      }
+    };
+    getData();
+  };
+
+  return <Input onChange={filterCharacters} />;
 };
